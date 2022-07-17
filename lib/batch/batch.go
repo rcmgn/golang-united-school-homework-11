8d95678ffea9653293a0bc7028f74e76891ae2ce
rcmgn <rcmgn@yandex.ru>
2022-07-17 20:08:28 +0500

    Test solution

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,7 +1,6 @@
 package batch
 
 import (
-	"fmt"
 	"sync"
 	"time"
 )
@@ -10,31 +9,41 @@ type user struct {
 	ID int64
 }
 
+type userCount struct {
+	sync.Mutex
+	u []user
+}
+
 func getOne(id int64) user {
 	time.Sleep(time.Millisecond * 100)
 	return user{ID: id}
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	var lock sync.Mutex
-	var wg sync.WaitGroup
-	ch := make(chan struct{}, pool)
+	//var lock sync.Mutex
+	//var wg sync.WaitGroup
+	ch := make(chan user, pool)
+	//var r userCount
 	var r []user
 	for i := int64(0); i < n; i++ {
-		ch <- struct{}{}
-		wg.Add(1)
-		go func(r1 *[]user) []user {
+		//wg.Add(1)
+		go func(i int64) {
 			user := getOne(i)
-			fmt.Println(user)
-			lock.Lock()
-			r = append(r, user)
-			lock.Unlock()
-			<-ch
-			wg.Done()
-			return *r1
-		}(&r)
+			//r.Mutex.Lock()
+			//defer r.Mutex.Unlock()
+			//r.u = append(r.u, user)
+			ch <- user
+			//wg.Done()
+		}(i)
 	}
-	wg.Wait()
+	//wg.Wait()
+	for i := int64(0); i < n; i++ {
+		var m user
+		m = <-ch
+		//fmt.Println(m)
+		r = append(r, m)
+
+	}
 	close(ch)
 	return r
 }
